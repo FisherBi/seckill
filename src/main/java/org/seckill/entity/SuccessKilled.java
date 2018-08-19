e852256b63d8da5028f8c7a5da1aefe8597d38d3
FisherBi <1015329590@qq.com>
2018-08-19 15:26:51 +0800

    add entity and dao.

---
+++ src/main/java/org/seckill/entity/SuccessKilled.java
@@ -0,0 +1,71 @@
+package org.seckill.entity;
+
+import java.util.Date;
+
+/**
+ * Created by FisherBi on 2018/8/19.
+ */
+public class SuccessKilled {
+
+    private long secKillId;
+
+    private long userPhone;
+
+    private short state;
+
+    private Date createTime;
+
+    //变通
+    //多对一
+    private SecKill secKill;
+
+    public long getSecKillId() {
+        return secKillId;
+    }
+
+    public void setSecKillId(long secKillId) {
+        this.secKillId = secKillId;
+    }
+
+    public long getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(long userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public short getState() {
+        return state;
+    }
+
+    public void setState(short state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public SecKill getSecKill() {
+        return secKill;
+    }
+
+    public void setSecKill(SecKill secKill) {
+        this.secKill = secKill;
+    }
+
+    @Override
+    public String toString() {
+        return "SuccessKilled{" +
+                "secKillId=" + secKillId +
+                ", userPhone=" + userPhone +
+                ", state=" + state +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
